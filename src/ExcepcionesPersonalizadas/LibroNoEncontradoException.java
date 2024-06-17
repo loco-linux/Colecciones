c6e388d49b818b9d4d8922e14e62510c27d2d8ee
JFBP <jai.barraza@duocuc.cl>
2024-06-16 23:43:09 -0400

    Desarrollo de Proyecto para Administracion de Biblioteca Duoc UC con uso de ArrayList, HashMap, TreeSet, HashSet.

---
+++ src/ExcepcionesPersonalizadas/LibroNoEncontradoException.java
@@ -0,0 +1,17 @@
+package ExcepcionesPersonalizadas;
+
+/*  Creado por Sebastian Olave y Jaime Barraza 
+    para POO I - Desarrollo de aplicaciones 
+    Duoc UC
+*/
+
+public class LibroNoEncontradoException extends Exception {
+    
+    public LibroNoEncontradoException(){
+    }
+    
+    public LibroNoEncontradoException (String msj_error) { // Manejo excepciones
+    super(msj_error);
+    }
+    
+}
